a29f3592b47f529df0b4857b473f9133fc85cdf9
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-05-16 23:33:13 +0300

    update pj

--- app/src/main/java/com/dimaskach/MainActivity.kt
+++ app/src/main/java/com/dimaskach/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.dimaskach
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-@SuppressLint("CustomSplashScreen")
-class SplashActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
