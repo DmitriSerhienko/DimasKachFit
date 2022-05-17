c946cae5ba728fe7c60dd5fcced039f6c1d79823
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-05-17 20:00:16 +0300

    update pj

--- app/src/main/java/com/dimaskach/adapters/DaysAdapter.kt
+++ app/src/main/java/com/dimaskach/adapters/DaysAdapter.kt
@@ -9,26 +9,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.dimaskach.R
 import com.dimaskach.databinding.DaysListItemBinding
 
-class DaysAdapter : ListAdapter<DayModel, DaysAdapter.DayHolder> (MyComparator()){
+class DaysAdapter() : ListAdapter<DayModel, DaysAdapter.DayHolder>(MyComparator()){
 
     class DayHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val binding = DaysListItemBinding.bind(view)
 
         fun setData(day: DayModel) = with(binding){
-            val name = root.context.getString(R.string.day) + "${adapterPosition + 1}"
+            val name = root.context.getString(R.string.day) + "${adapterPosition + 1}  "
             tvName.text = name
-            val exCounter = day.exercises.split(",0,").size.toString()
+            val exCounter = day.exercises.split(",").size.toString() +
+                    root.context.getString(R.string.exercise)
             tvExCounter.text = exCounter
+
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.days_list_item, parent, false)
+        val view = LayoutInflater.from(parent.context).
+        inflate(R.layout.days_list_item, parent, false)
         return DayHolder(view)
     }
 
     override fun onBindViewHolder(holder: DayHolder, position: Int) {
-        holder.setData(getItem(position))
+        holder.setData(getItem(position) )
     }
     class MyComparator: DiffUtil.ItemCallback<DayModel> () {
         override fun areItemsTheSame(oldItem: DayModel, newItem: DayModel): Boolean {
@@ -38,6 +41,7 @@ class DaysAdapter : ListAdapter<DayModel, DaysAdapter.DayHolder> (MyComparator()
         override fun areContentsTheSame(oldItem: DayModel, newItem: DayModel): Boolean {
             return oldItem == newItem
         }
-
     }
+
+
 }
--- app/src/main/java/com/dimaskach/fragments/DaysFragment.kt
+++ app/src/main/java/com/dimaskach/fragments/DaysFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dimaskach.R
+import com.dimaskach.adapters.DayModel
+import com.dimaskach.adapters.DaysAdapter
 import com.dimaskach.databinding.FragmentDaysBinding
+import com.dimaskach.utils.FragmentManager
 
 class DaysFragment : Fragment() {
     private lateinit var binding: FragmentDaysBinding
@@ -19,10 +24,33 @@ class DaysFragment : Fragment() {
         return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
 
+
+    private fun initRcView() = with(binding) {
+        val adapter = DaysAdapter()
+        rcViewDays.layoutManager = LinearLayoutManager(activity as AppCompatActivity)
+        rcViewDays.adapter = adapter
+        adapter.submitList(fillDaysArray())
+    }
+
+    private fun fillDaysArray(): ArrayList<DayModel>{
+        val tArray = ArrayList<DayModel>()
+        resources.getStringArray(R.array.day_exercises).forEach{
+            tArray.add(DayModel(it, false))
+        }
+        return tArray
+    }
+
+    companion object {
         @JvmStatic
         fun newInstance() = DaysFragment()
-            }
+    }
+
+
+
 
 }
